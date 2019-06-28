bb2ff8998f4c5a01ed6176866e614459c697133c
Robert-Vaccaro <48065298+Robert-Vaccaro@users.noreply.github.com>
2019-06-28 14:29:13 -0700

    Initial STORE js

---
+++ datastore.js
@@ -0,0 +1,122 @@
+const STORE = [
+    {
+      question: 'How did Daenerys Targaryen eventually hatch her dragon eggs?',
+      answers: [
+        'In a lightning storm',
+        'In a funeral pyre',
+        'In a fireplace',
+        'In a frozen cave'
+        ],
+      correctAnswer: 'In a funeral pyre',
+      icon: 'https://media.fromthegrapevine.com/assets/images/2016/1/dragon-eggs-game-of-thrones.jpg.482x490_q71_crop-smart.jpg',
+      alt: 'Dragon Eggs'
+    },
+    {
+      question: 'What is the only thing that can put out volatile Wildfire?',
+      answers: [
+        'Sand',
+        'Water',
+        'Dragon\'s blood',
+        'Sunlight'
+        ],
+        correctAnswer: 'Sand',
+        icon: 'https://media.fromthegrapevine.com/assets/images/2016/1/wildfire-game-of-thrones.jpg.482x490_q71_crop-smart.jpg',
+        alt: 'Wildfire'
+    },
+    {
+      question: 'Besides dragonglass, what is the only other substance capable of defeating White Walkers?',
+      answers: [
+        'Weirwood',
+        'Wildfire',
+        'Valyrian Steel',
+        'Snowballs'
+        ],
+        correctAnswer: 'Valyrian Steel',
+        icon: 'https://www.pedestrian.tv/content/uploads/2014/05/Podrick-Payne-Comic-Con-Melbourne-619-386.jpg',
+        alt: 'Podrick'
+    },
+    {
+      question: 'How many times has Beric Dondarrion been brought back to life?',
+      answers: [
+        'Three times',
+        'Five times',
+        'Six times',
+        'Seven times'
+        ],
+      correctAnswer: 'Six times',
+      icon: 'https://media.fromthegrapevine.com/assets/images/2016/1/Beric-Dondarrion.jpg.482x490_q71_crop-smart.jpg',
+      alt: 'Beric Dondarrion'
+    },
+    {
+      question: 'Arya\'s punishment for stealing from the Many-Face God is:',
+      answers: [
+        'Death',
+        'Memory Loss',
+        'Blindness',
+        'Uncontrollable laughter'
+        ],
+      correctAnswer: 'Blindness',
+      icon: 'https://media.fromthegrapevine.com/assets/images/2016/1/arya-stark.jpg.482x490_q71_crop-smart.jpg',
+      alt: 'Arya Stark'
+    },
+    {
+      question: '\'It\'s nothing\' were the last words of this infamous character:',
+      answers: [
+        'Renly Baratheon',
+        'Tywin Lannister',
+        'Robb Stark',
+        'King Joffrey'
+        ],
+      correctAnswer: 'King Joffrey',
+      icon: 'https://imgix.bustle.com/elite-daily/2017/07/28103007/joffrey-game-of-thrones-choking.jpg?w=1020&h=574&fit=crop&crop=faces&auto=format&q=70',
+      alt: 'King Joffrey'
+    },
+    {
+      question: 'What was the name of Ned Stark\s greatsword?',
+      answers: [
+        'Ice',
+        'Oathkeeper',
+        'Widow\'s Wail',
+        'Northguard'
+        ],
+      correctAnswer: 'Ice',
+      icon: 'https://cdn-static.denofgeek.com/sites/denofgeek/files/styles/main_wide/public/2018/05/game-of-thrones-season-1-recap.jpg?itok=ZwQDxPFR',
+      alt: 'Ned Stark'
+    },
+    {
+      question: 'Who shoots the flaming arrow that subsequently destroy\'s Stannis\' fleet in Blackwater Bay?',
+      answers: [
+        'Tyrion Lannister',
+        'King Joffrey',
+        'Jaime Lannister',
+        'Bronn'
+        ],
+      correctAnswer: 'Bronn',
+      icon: 'https://media.fromthegrapevine.com/assets/images/2016/1/blackwater-bay.jpg.482x490_q71_crop-smart.jpg',
+      alt: 'blackwater bay fire'
+    },
+    {
+      question: 'Who created the secret tunnel in the sewers under Casterly Rock?',
+      answers: [
+        'Tyrion Lannister',
+        'Lord Baelish',
+        'Jaime Lannister',
+        'Varys'
+        ],
+      correctAnswer: 'Tyrion Lannister',
+      icon: 'https://media.fromthegrapevine.com/assets/images/2017/9/casterly-rock.jpg.482x490_q71_crop-smart.jpg',
+      alt: 'Casterly Rock'
+    },
+    {
+      question: 'Dead creatures revived by White Walkers are known as:',
+      answers: [
+        'Walkers',
+        'Wights',
+        'Zombie',
+        'Claws'
+        ],
+    correctAnswer: 'Wights',
+    icon: 'https://cdn.konbini.com/wp-content/blogs.dir/13/files/2017/10/sam-feat-480x279.jpg',
+    alt: 'Samwell Tarly'
+    }
+];
